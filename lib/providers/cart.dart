99ddf3562aa7318f8c85d56938d3b829cd71fd60
“mkn1495” <manojnayakbellala@live.com>
2020-10-18 00:22:08 +0530

    Added expanded card for ordered items for more information on orders, added manage products screen Oct17_Sec9_V216

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -63,6 +63,26 @@ class Cart with ChangeNotifier {
     notifyListeners();
   }
 
+  void removeSingleItem(String prodId) {
+    if (!_items.containsKey(prodId)) {
+      return;
+    }
+    if (_items[prodId].quantity > 1) {
+      _items.update(
+        prodId,
+        (existingCartItem) => CartItem(
+          id: existingCartItem.id,
+          title: existingCartItem.title,
+          price: existingCartItem.price,
+          quantity: existingCartItem.quantity - 1,
+        ),
+      );
+    } else {
+      _items.remove(prodId);
+    }
+    notifyListeners();
+  }
+
   void clear() {
     _items = {};
     notifyListeners();
